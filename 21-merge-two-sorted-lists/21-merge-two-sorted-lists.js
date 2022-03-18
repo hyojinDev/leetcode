8cd651e596b70479a5a0190995d3e26a3a81dd2d
이효진 (Lee Hyojin) <gywls3394@gmail.com>
2022-03-18 15:51:29 +0900

    Time: 90 ms (61.42%), Space: 44.2 MB (55.85%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,33 +11,24 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let newList = new ListNode(0);
-    let head = newList;
-    let index = 1;
-    while (list1 != null && list2 != null) {
-        // console.log('while : ', list1, list2, newList)
-        if (list1.val < list2.val) {
-            // console.log('A',index, newList, newList.next, list1, list2, head)
-            newList.next = list1;
+    let newList = new ListNode();
+    let current = newList;
+    
+    while(list1 && list2) {
+        if(list1.val <= list2.val) {
+            current.next = new ListNode(list1.val);
+//             현재 head(list1.val)노드가 current.next로 할당되었음으로 next값이 Head로 옮겨져야함
             list1 = list1.next;
-             console.log('B', index, newList, newList.next, list1, list2, head)
         } else {
-             // console.log('C',index, newList, newList.next, list1, list2, head)
-            newList.next = list2;
+            current.next = new ListNode(list2.val);
             list2 = list2.next;
-             console.log('D',index, newList, newList.next, list1, list2, head)
+
         }
-         // console.log('E',index, newList, newList.next, list1, list2, head)
-        newList = newList.next;
-         console.log('F',index, newList, newList.next, list1, list2, head)
-        index++;
+//         current값은 항상 더 큰값으로 갱신해주어야 하기 때문에 조건문이 끝날 때마다 재할당
+        current = current.next;
     }
-    console.log('Final',index, newList, newList.next, list1, list2, head)
-    if (list1 === null) {
-        newList.next = list2;
-    }
-    else {
-        newList.next = list1;
-    }
-    return head.next;
+//     둘 중 하나가 null 값일 수 있음으로 마지막으로 tail노드에 할당해줌
+    current.next = list1 || list2;
+//     처음에 new ListNode() 생성할 때 들어간 0을 제외하고 return 해야함
+    return newList.next;
 };
