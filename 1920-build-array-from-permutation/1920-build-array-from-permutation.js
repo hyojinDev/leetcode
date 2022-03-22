171472b3d045f31b0328175da85b2259a61f8d1f
이효진 (Lee Hyojin) <gywls3394@gmail.com>
2022-03-22 19:26:39 +0900

    Time: 104 ms (61.89%), Space: 45.3 MB (79.40%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -3,10 +3,10 @@
  * @return {number[]}
  */
 var buildArray = function(nums) {
-    let result = [];
-    for(let i = 0; i < nums.length; i++){
-        result.push(nums[nums[i]]);
-    }
-    // let result = nums.map((item, idx) => nums[item])
+    // let result = [];
+    // for(let i = 0; i < nums.length; i++){
+    //     result.push(nums[nums[i]]);
+    // }
+    let result = nums.map((item, idx) => nums[item])
     return result;
 };
